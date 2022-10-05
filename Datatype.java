f2f667d6cc385f30dc601e2104c6bf80bffb4681
gowthaman0304 <gowthamg099@gmail.com>
2022-10-05 21:43:49 +0530

    upload my datatype code

---
+++ Datatype.java
@@ -0,0 +1,31 @@
+class Datatype
+{
+public static void main(String[] args){
+
+
+byte b=125;
+
+short s=5678;
+
+int i=65123;
+
+long l=123487;
+
+float f=9.27f;
+
+double d=7.3;
+
+char c='g';
+
+
+System.out.println("byte: " + b);
+System.out.println("short: " + s);
+System.out.println("integer: " + i);
+System.out.println("long: " + l);
+System.out.println("float: " + f);
+System.out.println("double: " + d);
+System.out.println("char: " + c);
+}
+}
+ 
+
