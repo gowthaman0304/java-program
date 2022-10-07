18f24b3a5a002eea4a2c6da90da4417a85657531
gowthaman0304 <gowthamg099@gmail.com>
2022-10-07 08:57:54 +0530

    upload my code

---
+++ Subject.java
@@ -0,0 +1,46 @@
+class Subject
+{
+public static void main(String[] args)
+{
+
+Subject sub=new Subject();
+
+sub.tamil();
+sub.english();
+sub.maths();
+sub.scince();
+sub.social();
+sub.total();
+
+
+}
+
+void tamil()
+{
+System.out.println(83);
+}
+void english()
+{
+System.out.println(63);
+}
+void maths()
+{
+System.out.println(88);
+}
+void scince()
+{
+System.out.println(89);
+}
+void social()
+{
+System.out.println(94);
+}
+
+void total()
+{
+System.out.println(83+63+88+89+94);
+}
+
+
+
+}
