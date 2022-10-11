79eea76cfcb3643dac32bb69da92298c41bcbdc3
gowthaman0304 <gowthamg099@gmail.com>
2022-10-11 18:31:52 +0530

    code reusability program

---
+++ Theatre.java
@@ -0,0 +1,22 @@
+class Theatre
+{
+void bookTicket(int amount)
+{
+System.out.println("Balance is"+(amount-120));
+
+}
+
+
+
+// class Customer 
+
+public static void main(String[] args)
+{
+
+Theatre theatre=new Theatre();
+
+theatre.bookTicket(200);
+}
+}
+
+
