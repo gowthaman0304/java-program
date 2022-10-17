7a1e6dc91d9d5b8dcf4f825a1771a2356d015392
gowthaman0304 <gowthamg099@gmail.com>
2022-10-17 18:22:32 +0530

    package using import key

---
+++ Student2.java
@@ -0,0 +1,20 @@
+package chennai.annanager;
+
+import chennai.poonthamallee.College;
+
+class Student2
+{
+
+public static void main(String [] args)
+{
+
+College college=new College();
+
+college.admission();
+college.admission_fees();
+
+}
+
+}
+
+
