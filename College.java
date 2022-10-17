77c4cc2fd22059e1a76a4426a00aa8194a4c6f01
gowthaman0304 <gowthamg099@gmail.com>
2022-10-17 18:12:45 +0530

    package program

---
+++ College.java
@@ -0,0 +1,20 @@
+package chennai.poonthamallee;
+
+public class College
+{
+
+public void admission()
+{
+
+System.out.println("Admission");
+
+}
+
+public void admission_fees()
+{
+
+System.out.println("Fees");
+
+}
+
+}
