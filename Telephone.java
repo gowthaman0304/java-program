bf07f2922ce5a3fe05b5d6b9f1025c83c2f0bbdc
gowthaman0304 <gowthamg099@gmail.com>
2022-10-18 19:07:10 +0530

    super class program

---
+++ Telephone.java
@@ -0,0 +1,31 @@
+class Telephone
+{
+
+String name ="phone";
+
+int price =1500;
+
+public void call()
+{
+System.out.println("Calling Facility");
+
+}
+
+public void receive_call()
+{
+System.out.println("Receiving call facility");
+
+}
+//public static void main(String[] args)
+//{
+
+//Telephone bsnl =new Telephone();
+
+//System.out.println(bsnl.name);
+//System.out.println(bsnl.price);
+
+//bsnl.call();
+//bsnl.receive_call();
+//}
+
+}
