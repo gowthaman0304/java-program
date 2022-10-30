9d653e7dbaf1336bf012a7c67eea9d2c81177919
gowthaman0304 <gowthamg099@gmail.com>
2022-10-30 13:48:48 +0530

    Assignment 1

---
+++ SQLTrainer.java
@@ -0,0 +1,15 @@
+class SQLTrainer extends Trainer
+{
+
+public static void main(String[] args)
+{
+SQLTrainer ram=new SQLTrainer();
+System.out.println(ram.getSalary());
+ram.training();
+
+
+System.out.println(ram.dept);
+System.out.println(ram.institute);
+}
+
+}
---
+++ Trainer.java
@@ -0,0 +1,34 @@
+class Trainer
+{
+String dept="java";
+String institute="payilagam";
+private int salary = 10000;
+
+int getSalary()
+{
+return this.salary;
+}
+
+Trainer(String dept1,String institute1)
+{
+{
+System.out.println("kumar dept is :" +dept1);
+System.out.println("kumar institute is :" +institute);
+}
+}
+
+Trainer()
+{
+}
+
+public static void main(String[] args)
+{
+Trainer trainerkumar=new Trainer("CSE","payilagam");
+}
+
+void training()
+{
+System.out.println("teaching java");
+}
+
+}
