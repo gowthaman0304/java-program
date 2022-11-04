b5ee6e51574bcc2fd190e434eb18e4db3c15a7a7
gowthaman0304 <gowthamg099@gmail.com>
2022-11-04 20:44:16 +0530

    Square numer

---
+++ Square_Number.java
@@ -0,0 +1,20 @@
+class Square_Number
+{
+public static void main(String[] args)
+{
+Square_Number sn = new Square_Number();
+sn.square_number();
+}
+void square_number()
+{
+int num = 1;
+while(num<=10)
+{
+int sum = num*num;
+System.out.println(sum);
+num = num+1;
+}
+
+}
+
+}
